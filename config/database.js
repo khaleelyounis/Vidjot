78329964a29a6580c07d8a7b7e242fb16d76c592
Khaleel Younis <khaleelyounis1@gmail.com>
2018-01-31 13:49:18 -0800

    Changed db password and about me page

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: 'mongodb://<Khaleel Younis>:<Ducks1994!>@ds121268.mlab.com:21268/vidjot-prod'
+        mongoURI: 'mongodb://<Khaleel Younis>:<Btrieu123!>@ds121268.mlab.com:21268/vidjot-prod'
     }
 } else {
     module.exports = {
