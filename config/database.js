de286d373021cbffd3844e6427ef6976c22c7b44
Khaleel Younis <khaleelyounis1@gmail.com>
2018-01-31 13:56:31 -0800

    final change of password for online db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: 'mongodb://<Khaleel Younis>:<Btrieu123!>@ds121268.mlab.com:21268/vidjot-prod'
+        mongoURI: 'mongodb://cal:thebestdatabaselol@ds121268.mlab.com:21268/vidjot-prod'
     }
 } else {
     module.exports = {
